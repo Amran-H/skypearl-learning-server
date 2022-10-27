934b93b7f0b8680750636652de01fb92e1bdb9f8
Amran Hossain <amran.h.akash@gmail.com>
2022-10-27 14:16:25 +0600

    fake data added

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 
 const categories = require('./data/categories.json');
+const course = require('./data/course.json');
 
 app.get('/', (req, res) => {
     res.send('My API is Running');
@@ -14,7 +15,28 @@ app.get('/', (req, res) => {
 
 app.get('/course-categories', (req, res) => {
     res.send(categories)
+});
+app.get('/course', (req, res) => {
+    res.send(course)
+});
+
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id;
+    if (id === '07') {
+        res.send(course);
+    }
+    else {
+        const category_course = course.filter(c => c.category_id === id);
+        res.send(category_course);
+    }
+
 })
+
+app.get('/course/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedCourse = course.find(c => c._id === id);
+    res.send(selectedCourse);
+});
 
 app.listen(port, () => {
     console.log('SkyPearl Learning server is running on port', port);
